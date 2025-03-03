73f8db32f5518c06a9dc8797c81e326f13306e11
Eric Engelhart <bob80333@gmail.com>
2025-03-03 14:59:24 -0500

    Add initial diffusion model code

---
+++ pitch_shifter/model/model_1d_v2_diffusion.py
@@ -0,0 +1,359 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchaudio.transforms as T
+from pitch_shifter.model.pixelshuffle1d import PixelUnshuffle1D, PixelShuffle1D
+from k_diffusion.layers import FourierFeatures
+
+# flatten weights for muon optimizer to work correctly
+
+class MyConv1d(nn.Conv1d):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.weight = nn.Parameter(self.weight.data.flatten(1, 2))
+
+    def forward(self, input: torch.Tensor):
+        return self._conv_forward(input, self.weight.view(self.out_channels, self.in_channels // self.groups, -1), self.bias)
+    
+
+class MyConvTranspose1d(nn.ConvTranspose1d):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.weight = nn.Parameter(self.weight.data.flatten(1, 2))
+
+    def forward(self, input: torch.Tensor, output_size=None):
+        if self.padding_mode != "zeros":
+            raise ValueError(
+                "Only `zeros` padding mode is supported for ConvTranspose1d"
+            )
+
+        assert isinstance(self.padding, tuple)
+        # One cannot replace List by Tuple or Sequence in "_output_padding" because
+        # TorchScript does not support `Sequence[T]` or `Tuple[T, ...]`.
+        num_spatial_dims = 1
+        output_padding = self._output_padding(
+            input,
+            output_size,
+            self.stride,  # type: ignore[arg-type]
+            self.padding,  # type: ignore[arg-type]
+            self.kernel_size,  # type: ignore[arg-type]
+            num_spatial_dims,
+            self.dilation,  # type: ignore[arg-type]
+        )
+        return F.conv_transpose1d(
+            input,
+            self.weight.view(self.in_channels, self.out_channels // self.groups, -1),
+            self.bias,
+            self.stride,
+            self.padding,
+            output_padding,
+            self.groups,
+            self.dilation,
+        )
+
+def snake(x, alpha):
+    shape = x.shape
+    x = x.reshape(shape[0], shape[1], -1)
+    x = x + (alpha + 1e-9).reciprocal() * torch.sin(alpha * x).pow(2)
+    x = x.reshape(shape)
+    return x
+
+
+class Snake1d(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        # channels last
+        self.alpha = nn.Parameter(torch.ones(1, 1, channels))
+
+    def forward(self, x):
+        return snake(x, self.alpha)
+
+
+class DownsampleWithSkip(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        factor=2,
+        skip="pixel_shuffle",
+        average_channels=2,
+    ):
+        super().__init__()
+        # calculate kernel size
+        # factor = 2 -> kernel_size = 3, padding = 1
+        # factor = 1 -> kernel_size = 1, padding = 0
+        if factor == 8:
+            kernel = 16
+        elif factor == 4:
+            kernel = 8
+        elif factor == 2:
+            kernel = 4
+        else:
+            kernel = 1
+        self.conv = MyConv1d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel,
+            stride=factor,
+            padding=factor // 2,
+        )
+
+        if skip == "pixel_shuffle":
+            self.skip = PixelUnshuffle1D(factor)
+        else:
+            self.skip = nn.Identity()
+
+        self.avg_channels = average_channels
+
+    def forward(self, x):
+        res = x
+        x = self.conv(x)
+        res = self.skip(res)
+        if self.avg_channels > 1:
+            # average avg_channels factor of channels
+            res = res.view(
+                res.size(0),
+                res.size(1) // self.avg_channels,
+                self.avg_channels,
+                res.size(2),
+            )
+            res = res.mean(dim=2)
+        return x + res
+
+
+class UpsampleWithSkip(nn.Module):
+    def __init__(
+        self, in_channels, out_channels, factor=2, skip="pixel_shuffle", dup_channels=2
+    ):
+        super().__init__()
+        # calculate kernel size
+        # factor = 2 -> kernel_size = 3, padding = 1
+        # factor = 1 -> kernel_size = 1, padding = 0
+        if factor == 8:
+            kernel = 16
+        elif factor == 4:
+            kernel = 8
+        elif factor == 2:
+            kernel = 4
+        else:
+            kernel = 1
+        self.conv = MyConvTranspose1d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel,
+            stride=factor,
+            padding=factor // 2,
+        )
+
+        if skip == "pixel_shuffle":
+            self.skip = PixelShuffle1D(factor)
+        else:
+            self.skip = nn.Identity()
+
+        self.dup_channels = dup_channels
+
+    def forward(self, x):
+        res = x
+        x = self.conv(x)
+        res = self.skip(res)
+        if self.dup_channels > 1:
+            # duplicate dup_channels factor of channels
+            res = res.view(res.size(0), res.size(1), 1, res.size(2))
+            res = res.expand(-1, -1, self.dup_channels, -1)
+            res = res.contiguous().view(
+                res.size(0), res.size(1) * self.dup_channels, res.size(3)
+            )
+
+        return x + res
+
+
+class ConvNextBlock(nn.Module):
+    def __init__(self, channels, expansion=4, layer_scale_init=1e-6, kernel=7):
+        super().__init__()
+
+        self.dw_conv = MyConv1d(
+            channels, channels, kernel_size=kernel, padding=(kernel-1)//2, groups=channels
+        )
+
+        #self.norm = nn.LayerNorm(channels)
+
+        self.pw_conv1 = nn.Linear(channels, channels * expansion)
+        self.pw_conv2 = nn.Linear(channels * expansion, channels)
+
+        self.act = nn.GELU()
+
+        self.gamma = (
+            nn.Parameter(torch.ones(channels) * layer_scale_init, requires_grad=True)
+            if layer_scale_init > 0
+            else None
+        )
+
+    def forward(self, x):
+        res = x
+        x = self.dw_conv(x)
+        x = x.permute(0, 2, 1).contiguous()
+        #x = self.norm(x)
+        x = self.pw_conv1(x)
+        x = self.act(x)
+        x = self.pw_conv2(x)
+
+        if self.gamma is not None:
+            x = x * self.gamma
+
+        x = x.permute(0, 2, 1).contiguous()
+
+        return x + res
+
+class Encoder(nn.Module):
+    def __init__(self, channels, blocks, factors, scale_vs_channels):
+        super().__init__()
+
+        kernels = [7, 23, 41]
+
+        self.blocks = nn.ModuleList()
+        for i in range(len(channels) - 1):
+            for j in range(blocks[i]):
+                self.blocks.append(ConvNextBlock(channels[i], kernel=kernels[j]))
+            self.blocks.append(
+                DownsampleWithSkip(
+                    channels[i],
+                    channels[i + 1],
+                    average_channels=scale_vs_channels[i],
+                    factor=factors[i],
+                )
+            )
+
+    def forward(self, x):
+        residuals = [x]
+        for block in self.blocks:
+            x = block(x)
+            if isinstance(block, DownsampleWithSkip):
+                residuals.append(x)
+
+        # remove last residual
+        residuals.pop()
+
+        return x, residuals
+
+
+class Decoder(nn.Module):
+    def __init__(self, channels, blocks, factors, scale_vs_channels):
+        super().__init__()
+
+        kernels = [7, 23, 41]
+
+        self.blocks = nn.ModuleList()
+        for i in range(1, len(channels)):
+            self.blocks.append(
+                UpsampleWithSkip(
+                    channels[i - 1],
+                    channels[i],
+                    dup_channels=scale_vs_channels[i - 1],
+                    factor=factors[i - 1],
+                )
+            )
+            for j in range(blocks[i - 1]):
+                self.blocks.append(ConvNextBlock(channels[i], kernel=kernels[j]))
+
+            self.blocks.append(MyConv1d(channels[i] * 2, channels[i], 1))
+
+
+    def forward(self, x, residuals):
+        for block in self.blocks:
+            # skip conv
+            if isinstance(block, MyConv1d):
+                x = torch.cat([x, residuals.pop()], dim=1)
+                
+            x = block(x)
+
+        return x
+
+
+class WavUNet(nn.Module):
+    def __init__(self, channels=None, blocks=None):
+        super().__init__()
+
+        if channels is None:
+            channels = [16, 32, 128, 256, 512]
+            blocks = [3, 3, 3, 3]
+            factors = [2, 4, 4, 8]
+            scale_vs_channels = [1, 1, 2, 4]
+
+            bottleneck_blocks = 3
+
+            patching = 2
+
+        self.encoder = Encoder(channels, blocks, factors, scale_vs_channels)
+        self.decoder = Decoder(
+            channels[::-1], blocks[::-1], factors[::-1], scale_vs_channels[::-1]
+        )
+
+        self.bottleneck = nn.Sequential(*[ConvNextBlock(channels[-1]) for _ in range(bottleneck_blocks)])
+
+        self.in_patch = PixelUnshuffle1D(patching)
+        self.out_patch = PixelShuffle1D(patching)
+
+        # take in 2 channels, output 1 channel
+        # 2 inputs: conditioning audio (shifted) and x_t audio
+        self.conv_in = MyConv1d(patching * 2, channels[0], 5, padding=2)
+        self.conv_out = MyConv1d(channels[0], patching, 5, padding=2)
+        
+        self.timestamp_mlp = nn.Sequential(
+            FourierFeatures(1, 128),
+            nn.Linear(128, 512),
+            nn.GELU(),
+            nn.Linear(512, 512),
+            nn.GELU(),
+            nn.Linear(512, channels[0])
+        )
+
+    def forward(self, x, timestamp):
+        ts_embed = self.timestamp_mlp(timestamp).unsqueeze(-1) # (B, C, T)
+        x = self.in_patch(x)
+        x = self.conv_in(x)
+        x = x + ts_embed
+        
+        x, residuals = self.encoder(x)
+        x = self.bottleneck(x)
+        x = self.decoder(x, residuals)
+        x = self.conv_out(x)
+        x = self.out_patch(x)
+
+        return x
+    
+    def apply_weightnorm(self):
+        # replace each conv1d and linear with weight normalized version
+        for module in self.modules():
+            if isinstance(module, (MyConv1d, nn.Linear)):
+                nn.utils.weight_norm(module)
+
+
+if __name__ == "__main__":
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = WavUNet().to(device)
+    print(model)
+    opt_model = torch.compile(model)
+    # tf32
+    torch.backends.cuda.matmul.allow_tf32 = True
+    # print # model params
+    params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print(f"# params: {params/1e6:.2f}M")
+    x = torch.randn(32, 1, 16384*2).to(device)
+    print(x.shape)
+    t = torch.rand(32, 1).to(device)
+    from time import time
+    from tqdm import trange
+    # warmup
+    with torch.no_grad():
+        for _ in trange(50):
+            x = torch.randn(32, 1, 16384*2).to(device)
+            y = opt_model(x, t)
+
+    with torch.no_grad():
+        start = time()
+        for _ in trange(200):
+            x = torch.randn(32, 1, 16384*2).to(device)
+            y = opt_model(x, t)
+        end = time()
+    print("Input shape", x.shape, "Output shape", y.shape)
+    print("Took", end - start, "seconds")
---
+++ scripts/train_unshift_down_diffusion.py
@@ -0,0 +1,327 @@
+from muon import Muon
+import torch
+from pitch_shifter.model.model_1d_v2 import WavUNet
+from pitch_shifter.data.data import PreShiftedDownAudioDataset
+from torch.utils.data import DataLoader
+from pathlib import Path
+import argparse
+from tqdm import trange
+from auraloss.freq import MultiResolutionSTFTLoss
+from auraloss.time import SISDRLoss
+from torch.utils.tensorboard import SummaryWriter
+import os
+import torchaudio.transforms as T
+from audiotools.metrics.spectral import MelSpectrogramLoss
+from audiotools.core.audio_signal import AudioSignal
+import numpy as np
+import random
+
+sr = 48_000
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# performance tweaks
+torch.backends.cudnn.benchmark = True
+torch.backends.cuda.matmul.allow_tf32 = True
+
+# based off of ddpm step code in lucidrains RIN repo
+
+def gamma(t, ns=0.0002, ds=0.00025):
+    return torch.cos(((t + ns) / (1 + ds)) * np.pi / 2)**2
+
+
+def gamma_to_alpha_sigma(gamma, scale=1):
+    return torch.sqrt(gamma) * scale, torch.sqrt(1 - gamma)
+
+
+def safe_div(numer, denom, eps=1e-10):
+    return numer / denom.clamp(min=eps)
+
+
+def ddpm_step_lucidrains(x_t, eps_pred, t_now, t_next):
+    t_now = torch.tensor(t_now, device=device)
+    t_next = torch.tensor(t_next, device=device)
+    gamma_now = gamma(t_now)
+    gamma_next = gamma(t_next)
+    alpha_now, sigma_now = gamma_to_alpha_sigma(gamma_now)
+    alpha_next, sigma_next = gamma_to_alpha_sigma(gamma_next)
+
+    # convert eps into x_0
+    x_start = safe_div(x_t - sigma_now * eps_pred, alpha_now)
+
+    # clip
+    x_start.clamp_(-1.0, 1.0)
+
+    # get predicted noise
+    pred_noise = safe_div(x_t - alpha_now * x_start, sigma_now)
+
+    # calculate next x_t
+    x_next = x_start * alpha_next + pred_noise * sigma_next
+    return x_next
+
+
+def generate(steps, noise, model, conditioning):
+    x_t = noise
+    for step in trange(steps):
+        # Get time for current and next states.
+        t = 1 - step / steps
+        timestep = torch.ones(x_t.shape[0], device=device) * t
+        t_m1 = max(1 - (step + 1) / steps, 0)
+        # Predict eps.
+        
+        model_in = torch.cat([x_t, conditioning], dim=1)
+        eps_pred = model(model_in, timestep)
+        # Estimate x at t_m1.
+        x_t = ddpm_step_lucidrains(x_t, eps_pred, t, t_m1)
+    return x_t
+
+# set seed for reproducibility
+def seed_worker(worker_id):
+    worker_seed = torch.initial_seed() % 2**32
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
+
+
+def inf_train_generator(train_loader):
+    while True:
+        for data in train_loader:
+            yield data
+
+
+def main(args):
+
+    # set seed for reproducibility
+    torch.manual_seed(0)
+    np.random.seed(0)
+    random.seed(0)
+    g = torch.Generator()
+    g.manual_seed(0)
+
+    model = WavUNet()
+    model.to(device)
+
+    opt_model = torch.compile(model)
+
+    # for newer version of Muon
+    # Find ≥2D parameters in the body of the network -- these will be optimized by Muon
+    muon_params = [p for p in model.parameters() if p.ndim >= 2]
+    # Find everything else -- these will be optimized by AdamW
+    adamw_params = [p for p in model.parameters() if p.ndim < 2]
+    # Create the optimizer
+    optimizers = [
+        Muon(muon_params, lr=5e-3, momentum=0.95, weight_decay=0.01),
+        torch.optim.AdamW(adamw_params, lr=5e-4, betas=(0.90, 0.95), weight_decay=0.01),
+    ]
+
+    # optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3, betas=(0.9, 0.95), weight_decay=0.01)
+
+    train_files = list(Path("dataset_dir/train_processed_unshift_down").rglob("*.wav"))
+    print(f"Found {len(train_files)} training files")
+    train_dataset = PreShiftedDownAudioDataset(train_files, samples=16384 * 2)
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=args.batch_size,
+        shuffle=True,
+        drop_last=True,
+        num_workers=args.n_workers,
+        persistent_workers=True,
+        worker_init_fn=seed_worker,
+        generator=g,
+    )
+
+    val_files = list(Path("dataset_dir/val_processed_unshift_down").rglob("*.wav"))
+    print(f"Found {len(val_files)} validation files")
+    val_dataset = PreShiftedDownAudioDataset(val_files, test=True, samples=16384 * 8)
+    val_dataloader = DataLoader(
+        val_dataset,
+        batch_size=args.batch_size,
+        shuffle=False,
+        drop_last=True,
+        num_workers=args.n_workers,
+        persistent_workers=True,
+        worker_init_fn=seed_worker,
+        generator=g,
+    )
+
+    stft_loss = MultiResolutionSTFTLoss(
+        fft_sizes=[4096, 2048, 1024],
+        hop_sizes=[480, 240, 120],
+        win_lengths=[2400, 1200, 600],
+        scale="mel",
+        n_bins=128,
+        sample_rate=sr,
+        perceptual_weighting=True,
+    )
+
+    sisdr_loss = SISDRLoss()
+    l1_loss_fn = torch.nn.L1Loss()
+
+    # cdpam_loss = cdpam.CDPAM(dev='cuda:0')
+
+    # resampler = T.Resample(48000, 22050).to(device)
+
+    # dac_loss = DACFeatureMatchingLoss(device)
+
+    # wavlm_loss = WavLMFeatureMatchingLoss(device)
+
+    # using setting from DAC base.yml:
+    # MelSpectrogramLoss.n_mels: [5, 10, 20, 40, 80, 160, 320]
+    # MelSpectrogramLoss.window_lengths: [32, 64, 128, 256, 512, 1024, 2048]
+    # MelSpectrogramLoss.mel_fmin: [0, 0, 0, 0, 0, 0, 0]
+    # MelSpectrogramLoss.mel_fmax: [null, null, null, null, null, null, null]
+    # MelSpectrogramLoss.pow: 1.0
+    # MelSpectrogramLoss.clamp_eps: 1.0e-5
+    # MelSpectrogramLoss.mag_weight: 0.0
+    melspec_loss = MelSpectrogramLoss(
+        n_mels=[5, 10, 20, 40, 80, 160, 320],
+        window_lengths=[32, 64, 128, 256, 512, 1024, 2048],
+        mel_fmin=[0, 0, 0, 0, 0, 0, 0],
+        mel_fmax=[None, None, None, None, None, None, None],
+        pow=1.0,
+        clamp_eps=1.0e-5,
+        mag_weight=0.0,
+    )
+
+    writer = SummaryWriter(args.save_dir)
+
+    train_gen = inf_train_generator(train_dataloader)
+
+    for step in trange(args.n_steps):
+        model.train()
+        # optimizer.zero_grad()
+        for opt in optimizers:
+            opt.zero_grad()
+
+        audio, shifted_audio = next(train_gen)
+        audio = audio.to(device)
+        shifted_audio = shifted_audio.to(device)
+
+        # add channels dimension for losses calculation
+        audio = audio.unsqueeze(1)
+        shifted_audio = shifted_audio.unsqueeze(1)
+
+        noise = torch.randn_like(shifted_audio)
+
+        timesteps = torch.rand(shifted_audio.shape[0], 1, 1, device=device)
+
+        # create input for model
+        x_t = audio * gamma(timesteps).sqrt() + noise * gamma(1 - timesteps).sqrt()
+
+        # combine input with shifted audio (conditioning)
+        conditioned_input = torch.cat([x_t, shifted_audio], dim=1)
+
+        # predict fixed input audio
+        unshifted_audio = opt_model(conditioned_input, timesteps)
+
+        # calculate stft error for unshifted audio, should not have artifacts from shifting and should be back to original pitch
+        # loss = stft_loss(unshifted_audio, audio)
+
+        l1_loss = l1_loss_fn(unshifted_audio, audio)
+
+        # loss = cdpam_loss.forward(resampler(audio), resampler(unshifted_audio)).mean()
+
+        # loss = dac_loss(unshifted_audio, audio)
+        # feature_loss = wavlm_loss(unshifted_audio, audio)
+
+        # make tensors AudioSignals for MelSpectrogramLoss (takes in tensors, so should preserve gradients)
+        audio = AudioSignal(audio, sr)
+        unshifted_audio = AudioSignal(unshifted_audio, sr)
+
+        mel_loss = melspec_loss(unshifted_audio, audio)
+
+        loss = mel_loss + 10 * l1_loss
+        # loss = l1_loss(unshifted_audio, audio)
+        loss.backward()
+        # log / clip grad norm
+        writer.add_scalar(
+            "train/grad_norm",
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1e3).item(),
+            step + 1,
+        )
+
+        # optimizer.step()
+        for opt in optimizers:
+            opt.step()
+
+        writer.add_scalar("train/loss", loss, step + 1)
+        writer.add_scalar("train/mel_loss", mel_loss, step + 1)
+        writer.add_scalar("train/l1_loss", l1_loss, step + 1)
+        # writer.add_scalar("train/wavlm_loss", feature_loss, step+1)
+
+        if (step + 1) % args.eval_every == 0:
+            model.eval()
+            val_loss = 0
+            with torch.no_grad():
+                total_val_loss = 0
+                total_val_sisdr = 0
+                total_shifted_loss = 0
+                i = 0
+                for audio, shifted_audio in val_dataloader:
+                    audio = audio.to(device)
+                    shifted_audio = shifted_audio.to(device)
+
+                    # add channels dimension for losses calculation
+                    audio = audio.unsqueeze(1)
+                    shifted_audio = shifted_audio.unsqueeze(1)
+                    
+                    
+                    
+
+                    # remove pitch artifacts from shifted audio
+                    #unshifted_audio = opt_model(shifted_audio)
+                    
+                    noise = torch.randn_like(audio)
+                    
+                    unshifted_audio = generate(50, noise, opt_model, shifted_audio)
+
+                    # calculate stft error for unshifted audio, should not have artifacts from shifting and should be back to original pitch
+                    val_loss = stft_loss(unshifted_audio, audio)
+                    # calculate sisdr loss
+                    val_sisdr = sisdr_loss(unshifted_audio, audio)
+                    # calculate the error for the shifted audio, should have artifacts from shifting, the model output should have a better error than this
+                    shifted_loss = stft_loss(shifted_audio, audio)
+
+                    total_val_loss += val_loss
+                    total_val_sisdr += val_sisdr
+                    total_shifted_loss += shifted_loss
+                    i += 1
+
+                total_val_loss /= i
+                total_shifted_loss /= i
+                total_val_sisdr /= i
+
+                writer.add_scalar("val/loss", total_val_loss, step + 1)
+                writer.add_scalar("val/sisdr", total_val_sisdr, step + 1)
+                # baseline, if model output is worse than this, it's not useful
+                writer.add_scalar("val/shifted_loss", total_shifted_loss, step + 1)
+
+                # save an example output
+                writer.add_audio("val/audio", audio[0], step + 1, sample_rate=sr)
+                writer.add_audio(
+                    "val/shifted_audio", shifted_audio[0], step + 1, sample_rate=sr
+                )
+                writer.add_audio(
+                    "val/unshifted_audio", unshifted_audio[0], step + 1, sample_rate=sr
+                )
+
+            print(f"Step {step+1}, val_loss: {total_val_loss}")
+
+            torch.save(
+                model.state_dict(), os.path.join(args.save_dir, f"model_{step+1}.pt")
+            )
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--n_steps", type=int, default=10_000)
+    argparser.add_argument("--eval_every", type=int, default=1000)
+    argparser.add_argument("--batch_size", type=int, default=32)
+    argparser.add_argument("--n_workers", type=int, default=6)
+    argparser.add_argument(
+        "--save_dir", type=str, default="runs/outputs_unshift_down_diffusion/output1"
+    )
+
+    args = argparser.parse_args()
+
+    os.makedirs(args.save_dir, exist_ok=True)
+
+    main(args)
